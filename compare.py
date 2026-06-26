254680bbdd99e85ec75142b6440085ef9a7d2806
Lorraine Rono <lorraine.rono@savannahcement.com>
2026-06-26 11:50:14 +0300

    Adding new file for python

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input('Whats X? '))
+y = int(input('Whats Y? '))
+
+if x>y:
+    print(f'{x} is greater than {y}')
+elif x<y:
+    print(f'{x} is less than {y}')
+else:
+    print(f'{x} is equal to {y}')
